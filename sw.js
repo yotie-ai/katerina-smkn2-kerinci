59619502ec89628954d2195e073324143a8587a2
yotie-ai <yotiendrianis56@guru.smk.belajar.id>
2026-09-21 00:04:59 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
-const CACHE="katerina-v3";const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
+const CACHE="katerina-v42";
+const ASSETS=["./","./index.html","./manifest.webmanifest"];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys().then(k=>
+      Promise.all(
+        k.filter(x=>x!==CACHE).map(x=>caches.delete(x))
+      )
+    ).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const c=r.clone();
+      caches.open(CACHE).then(x=>x.put(e.request,c));
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
