90a7b8e7813e4066300dc7e0909eefa797050dab
yotie-ai <yotiendrianis56@guru.smk.belajar.id>
2026-09-21 09:30:39 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,25 @@
-const CACHE="katerina-v42";
-const ASSETS=["./","./index.html","./manifest.webmanifest"];
+const CACHE="katerina-v43";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 
 self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate",e=>{
   e.waitUntil(
-    caches.keys().then(k=>
-      Promise.all(
-        k.filter(x=>x!==CACHE).map(x=>caches.delete(x))
-      )
-    ).then(()=>self.clients.claim())
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    )).then(()=>self.clients.claim())
   );
 });
 
 self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
   e.respondWith(
     fetch(e.request).then(r=>{
-      const c=r.clone();
-      caches.open(CACHE).then(x=>x.put(e.request,c));
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
       return r;
     }).catch(()=>caches.match(e.request))
   );
